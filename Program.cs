0f37209c909537100dda1e3a308662df9c9fa786
Mine3e <minememmedlii@gmail.com>
2024-03-10 12:51:09 +0400

    task2

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        #region task1 
+        //int num = 123;
+        //int mul = 1;
+        //while (num > 0)
+        //{
+        //    int reqem = num % 10;
+        //    mul *= reqem;
+        //    num /= 10;
+        //}
+        //Console.WriteLine(mul);
+        #endregion
+        #region task2
+        //int eded = 11;
+        //bool sade = true;
+        //for (int i = 2; i < eded; i++)
+        //{
+        //    if (eded % i == 0)
+        //    {
+        //        sade = false;
+        //        break;
+        //    }
+        //}
+        //Console.WriteLine(sade);
+        #endregion
+        #region task3 
+        //int eded = 4;
+        //int mul = 1;
+        //for(int i = 1; i <= eded; i++)
+        //{
+        //    mul *= i;
+        //}
+        //Console.WriteLine(mul);
+        #endregion
+        #region task4
+        //int eded = 1221;
+        //int ededintersi = 0;
+        //int yenieded = eded;
+        //while (yenieded > 0)
+        //{
+        //    int qaliq = yenieded % 10;
+        //    ededintersi = (ededintersi * 10) + qaliq;
+        //    yenieded /= 10;
+        //}
+        //    if (eded == ededintersi)
+        //    {
+        //        Console.WriteLine("polindromdur");
+        //    }
+
+        //    else
+        //    {
+        //        Console.WriteLine("polindrom deyil");
+        //    }
+        
+        #endregion
+
+    }
+}
